7045a35b7fbcce2063feb05b8eaf71cbc23cbd4d
Sayak Tripathy <64369143+bubka22@users.noreply.github.com>
2021-05-19 16:22:17 +0530

    Sayak
    Sayak

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+function dis(val) 
+{
+	document.getElementById('text').value+=val;
+
+}
+
+
+function clr()
+{
+	document.getElementById('text').value=" ";
+}
+
+
+function solve()
+{
+	let x = document.getElementById('text').value;
+	let y = eval(x)
+	document.getElementById('text').value=y;
+}
